a12c02995401238dde83cd0f6ca375a1c9053f45
Vincent Rémond <vincent.remond@gmail.com>
2025-01-01 22:09:15 +0100

    v1

---
+++ AddToPath/Cli.fs
@@ -0,0 +1,37 @@
+﻿namespace AddToPath
+
+open Fargo
+open Fargo.Operators
+
+[<RequireQualifiedAccess>]
+module Cli =
+
+    [<RequireQualifiedAccess>]
+    type private Command' =
+        | Add
+        | List
+        | Check
+
+    type Command =
+        | Add of string
+        | List
+        | Check
+
+    let parser =
+        fargo {
+            match!
+                (cmd "add" null "Add a new path to the PATH user environment variable"
+                 |>> Command'.Add)
+                <|> (cmd "list" null "List the current PATH user environment variable"
+                     |>> Command'.List)
+                <|> (cmd "check" null "Check the current PATH user environment variable"
+                     |>> Command'.Check)
+                <|> (ret Command'.Add)
+            with
+            | Command'.List -> return Command.List
+            | Command'.Check -> return Command.Check
+            | Command'.Add
+            | _ ->
+                let! path = arg "path" "The path to add to the PATH user environment variable" |> reqArg
+                return Command.Add path
+        }
--- AddToPath/Program.fs
+++ AddToPath/Program.fs
@@ -3,52 +3,88 @@
 open System
 open AddToPath
 open Pinicola.FSharp
+open Pinicola.FSharp.Fargo
 open Pinicola.FSharp.SpectreConsole
 open Pinicola.FSharp.IO
 
 let envVarName = "PATH"
 
-let userEnvVarRawValue =
-    Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User)
+let readValues () =
+    let userEnvVarRawValue =
+        Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User)
 
-let initialValues = userEnvVarRawValue |> String.split ';' |> List.map String.trim
+    let values = userEnvVarRawValue |> String.split ';' |> List.map String.trim
+    values
 
-let fixValue v =
-    v
-    |> String.trimEndChar '\\'
-    |> (fun v ->
-        match Directory.exists v with
-        | true -> Some v
-        | false -> None
+let add pathToAdd =
+
+    let initialValues = readValues ()
+
+    let newValues =
+        match pathToAdd with
+        | Some p ->
+            let fullPath = Path.getFullPath p
+            fullPath :: initialValues
+        | None -> initialValues
+
+    let fixValue v =
+        v
+        |> String.trimEndChar '\\'
+        |> (fun v ->
+            match Directory.exists v with
+            | true -> Some v
+            | false -> None
+        )
+
+    let fixedValues =
+        newValues
+        |> List.choose fixValue
+        |> List.distinctBy String.toLower
+        |> List.sortBy (fun s ->
+            let sortKey =
+                match s with
+                | Contains StringComparison.OrdinalIgnoreCase @"\Inkscape\" -> 3
+                | Contains StringComparison.OrdinalIgnoreCase @"\Microsoft\WindowsApps" -> 2
+                | Contains StringComparison.OrdinalIgnoreCase @"\JetBrains\Toolbox\" -> 3
+                | _ -> 0
+
+            (sortKey, s |> String.toLower)
+        )
+
+    if fixedValues = initialValues then
+        AnsiConsole.markupLineInterpolated $"No changes needed in current [green]{initialValues.Length}[/] entries"
+        Environment.Exit 0
+
+    let diff = Diff.build initialValues fixedValues
+
+    for d in diff do
+        match d with
+        | Diff.Deleted v -> AnsiConsole.markupLineInterpolated $"[red]{v}[/]"
+        | Diff.Inserted v -> AnsiConsole.markupLineInterpolated $"[green]{v}[/]"
+        | Diff.Modified v -> AnsiConsole.markupLineInterpolated $"[yellow]{v}[/]"
+
+    let newValueAsString = fixedValues |> String.concat ";"
+
+    Environment.SetEnvironmentVariable(envVarName, newValueAsString, EnvironmentVariableTarget.User)
+
+    AnsiConsole.markupLineInterpolated $"[green]Done![/] New value has {fixedValues.Length} entries. [yellow]Restart your shell to apply changes[/]"
+
+let list () =
+    let values = readValues ()
+    AnsiConsole.markupLineInterpolated $"Current value has [green]{values.Length}[/] entries"
+    () |> Rule.initBlank |> AnsiConsole.write
+
+    for v in values do
+        AnsiConsole.markupLineInterpolated $" • {v}"
+
+    () |> Rule.initBlank |> AnsiConsole.write
+
+FargoCmdLine.run
+    "AddToPath"
+    Cli.parser
+    (fun args ->
+        match args with
+        | Cli.Command.Check -> add None
+        | Cli.Command.List -> list ()
+        | Cli.Command.Add path -> add (Some path)
     )
-
-let fixedValues =
-    initialValues
-    |> List.choose fixValue
-    |> List.distinctBy String.toLower
-    |> List.sortBy (fun s ->
-        let sortKey =
-            match s with
-            | Contains StringComparison.OrdinalIgnoreCase @"\Inkscape\" -> 2
-            | _ -> 1
-
-        (sortKey, s)
-    )
-
-if fixedValues = initialValues then
-    AnsiConsole.markupLineInterpolated $"No changes needed in current [green]{initialValues.Length}[/] entries"
-    Environment.Exit 0
-
-let diff = Diff.build initialValues fixedValues
-
-for d in diff do
-    match d with
-    | Diff.Deleted v -> AnsiConsole.markupLineInterpolated $"[red]{v}[/]"
-    | Diff.Inserted v -> AnsiConsole.markupLineInterpolated $"[green]{v}[/]"
-    | Diff.Modified v -> AnsiConsole.markupLineInterpolated $"[yellow]{v}[/]"
-
-let newValueAsString = fixedValues |> String.concat ";"
-
-Environment.SetEnvironmentVariable(envVarName, newValueAsString, EnvironmentVariableTarget.User)
-
-AnsiConsole.markupLineInterpolated $"[green]Done![/] New value has {fixedValues.Length} entries. [yellow]Restart your shell to apply changes[/]"
