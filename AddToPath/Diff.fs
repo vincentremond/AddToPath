cfa215fc4de9a485d69f841bcf89566fc3cc96cf
Vincent Rémond <vincent.remond@gmail.com>
2025-01-01 21:21:01 +0100

    first commit

---
+++ AddToPath/Diff.fs
@@ -0,0 +1,30 @@
+﻿namespace AddToPath
+
+open DiffPlex.DiffBuilder
+
+[<RequireQualifiedAccess>]
+module Diff =
+
+    type ChangeType =
+        | Deleted of string
+        | Inserted of string
+        | Modified of string
+
+    let private asText (x: string list) : string = x |> String.concat "\n"
+
+    let build old_ new_ =
+        let oldText = asText old_
+        let newText = asText new_
+
+        let diffs = InlineDiffBuilder.Diff(oldText, newText).Lines |> List.ofSeq
+
+        diffs
+        |> List.choose (fun line ->
+            match line.Type with
+            | DiffPlex.DiffBuilder.Model.ChangeType.Unchanged -> None
+            | DiffPlex.DiffBuilder.Model.ChangeType.Deleted -> Some <| Deleted line.Text
+            | DiffPlex.DiffBuilder.Model.ChangeType.Inserted -> Some <| Inserted line.Text
+            | DiffPlex.DiffBuilder.Model.ChangeType.Imaginary -> failwith "Unexpected Imaginary change type"
+            | DiffPlex.DiffBuilder.Model.ChangeType.Modified -> Some <| Modified line.Text
+            | _ -> failwith $"Unexpected change type: {line.Type}"
+        )
---
+++ AddToPath/Program.fs
@@ -0,0 +1,54 @@
+﻿module Program
+
+open System
+open AddToPath
+open Pinicola.FSharp
+open Pinicola.FSharp.SpectreConsole
+open Pinicola.FSharp.IO
+
+let envVarName = "PATH"
+
+let userEnvVarRawValue =
+    Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User)
+
+let initialValues = userEnvVarRawValue |> String.split ';' |> List.map String.trim
+
+let fixValue v =
+    v
+    |> String.trimEndChar '\\'
+    |> (fun v ->
+        match Directory.exists v with
+        | true -> Some v
+        | false -> None
+    )
+
+let fixedValues =
+    initialValues
+    |> List.choose fixValue
+    |> List.distinctBy String.toLower
+    |> List.sortBy (fun s ->
+        let sortKey =
+            match s with
+            | Contains StringComparison.OrdinalIgnoreCase @"\Inkscape\" -> 2
+            | _ -> 1
+
+        (sortKey, s)
+    )
+
+if fixedValues = initialValues then
+    AnsiConsole.markupLineInterpolated $"No changes needed in current [green]{initialValues.Length}[/] entries"
+    Environment.Exit 0
+
+let diff = Diff.build initialValues fixedValues
+
+for d in diff do
+    match d with
+    | Diff.Deleted v -> AnsiConsole.markupLineInterpolated $"[red]{v}[/]"
+    | Diff.Inserted v -> AnsiConsole.markupLineInterpolated $"[green]{v}[/]"
+    | Diff.Modified v -> AnsiConsole.markupLineInterpolated $"[yellow]{v}[/]"
+
+let newValueAsString = fixedValues |> String.concat ";"
+
+Environment.SetEnvironmentVariable(envVarName, newValueAsString, EnvironmentVariableTarget.User)
+
+AnsiConsole.markupLineInterpolated $"[green]Done![/] New value has {fixedValues.Length} entries. [yellow]Restart your shell to apply changes[/]"
